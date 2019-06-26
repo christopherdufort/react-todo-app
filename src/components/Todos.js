7f9cd51a70f9a22375080af8fd988df17f8c5eea
Christopher Dufort <christopher.dufort@osedea.com>
2019-06-26 17:30:27 -0400

    first display of state + cleanup of css

---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+// Upcase each word in the name by convention
+import React , { Component} from 'react';
+
+class Todos extends Component {
+  render(){
+    return(
+      <div >
+        <h1>Todos</h1>
+      </div>
+    );
+  }
+}
+
+export default Todos;
